37ac7dfc7e8b605442de48b6fa44e48561eb856a
Stephane B <besset.stephane@gmail.com>
2024-07-05 17:26:22 +0200

    work in progress

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_05_122023) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_05_140625) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "paniers", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_paniers_on_user_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "date"
+    t.bigint "user_id", null: false
+    t.bigint "panier_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["panier_id"], name: "index_reservations_on_panier_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +46,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_05_122023) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "paniers", "users"
+  add_foreign_key "reservations", "paniers"
+  add_foreign_key "reservations", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.destroy_all
+Panier.destroy_all
+Reservation.destroy_all
+
+User.create!(email: "stef@terre.fr", password: "azerty")
+
+Panier.create!(user_id: User.first.id, name: "Panier 1", description: "Panier de légumes petit", price: 15)
+Panier.create!(user_id: User.first.id, name: "Panier 2", description: "Panier de fruits petit", price: 15)
+Panier.create!(user_id: User.first.id, name: "Panier 3", description: "Panier de légumes grand", price: 25)
+Panier.create!(user_id: User.first.id, name: "Panier 4", description: "Panier de fruits grand", price: 25)
